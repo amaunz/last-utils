887f13064434c7e4bc626ef2a3772ff507677853
Andreas Maunz <andreas@maunz.de>
2009-11-18 14:07:04 +0100

    Completed SMARTS pattern

--- last-utils.rb
+++ last-utils.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 
+require('openbabel')
+#include OpenBabel   <= may be used for direct access to OB namespace, i.e. w/o "OpenBabel::". Below, I want namespaces for clarity.
+
 %w[pp rubygems hpricot].each { |x| require x }
 
 class LUGraph
@@ -7,36 +10,113 @@ class LUGraph
   def initialize(nodes, edges)
     @nodes = nodes
     @edges = edges
+  end
 
-    def to_smarts(f,opt)
-        if !@nodes[f].nil? 
-            @nodes[f].to_smarts 
-        else 
-            puts "No Node #{f}."
-            pp @nodes
-        end
-        @edges[f].each do |t,e|
+  def to_smarts(backw,f,opt)
 
-                # handle deleted
-                if (e.del_e == 0)
+      mand_branches=0
+      opt_branches = @edges[f].size
+      mand_t = []
+      mand_e = []
+      opt_t = []
+      opt_e = []
+      @edges[f].each do |t,e|
+          if e.opt_e==0 || (e.opt_e==opt)
+              mand_branches+=1
+              opt_branches-=1
+              mand_t << t
+              mand_e << e
+          else
+              opt_t << t
+              opt_e << e
+          end
+      end
 
-                    # handle opt open 
-                    opt_flag=false
-                    (opt_flag=true; print "["; opt=e.opt_e) unless e.opt_e<=opt 
-                    print "(" unless @edges[f].size==1
+      print "["
+      @nodes[f].to_smarts
+      do_branch=(opt_branches>1)
+      if do_branch
+          # 1) Uncomment next line to not force at least one branch (c.f. not. 17.11.09)
+          # print "$(" ; @nodes[f].to_smarts ; print ")," 
+          # 1) end
+          print ";"
+          for i in 0..opt_t.size-1
+              t = opt_t[i]
+              e = opt_e[i]
+              print "$("
+                  print "["                   # self
+                  @nodes[f].to_smarts         # 
+                  print "]"                   # 
 
-                    # recurse
-                    e.to_smarts
-                    to_smarts(t,opt)
-                    print ")" unless @edges[f].size==1
+                  print "("                   # branch
+                  e.to_smarts                 # 
+                  to_smarts(f,t,e.opt_e)      # 
+                  print ")"                   # 
+                  if backw!=f
+                      print "(" unless mand_branches==0   # backw
+                      print "["                           # backw
+                      @nodes[backw].to_smarts             # 
+                      print "]"                           # 
+                      print ")" unless mand_branches==0   # 
+                  end
+                  if mand_branches>0
+                      print "["               # forw
+                      @nodes[t].to_smarts     # 
+                      print "]"               # 
+                  end
+              print ")"
+              print "," unless i==opt_t.size-1
+          end
+      else
+          # 2) Uncomment next block to make single optional edges mandatory (c.f. not. 17.11.09)
+          #if opt_branches==1
+          #    @edges[f].each do |t,e|
+          #        if !mand_t.include?(t)
+          #            print "("
+          #            e.to_smarts
+          #            to_smarts(t,e.opt_e)
+          #            print ")"
+          #        end
+          #    end
+          #end
+          # 2) end
+      end
+      print "]"
+      print "(~*)" unless !do_branch
 
-                    # handle opt close
-                    print "]" unless !opt_flag
+      for i in 0..mand_t.size-1
+          t = mand_t[i]
+          e = mand_e[i]
+          print "(" unless i==mand_t.size-1
+          e.to_smarts
+          to_smarts(f,t,e.opt_e)
+          print ")" unless i==mand_t.size-1
+      end
 
-                end # end handle deleted
-            end
-        end
-    end
+  end
+
+
+#        @edges[f].each do |t,e|
+#
+#                # handle deleted
+#                if (e.del_e == 0)
+#
+#                    # handle opt open 
+#                    opt_flag=false
+#                    (opt_flag=true; print "["; opt=e.opt_e) unless e.opt_e<=opt 
+#                    print "(" unless @edges[f].size==1
+#
+#                    # recurse
+#                    e.to_smarts
+#                    to_smarts(t,opt)
+#                    print ")" unless @edges[f].size==1
+#
+#                    # handle opt close
+#                    print "]" unless !opt_flag
+#
+#                end # end handle deleted
+#            end
+#        end
 end
 
 class LUNode
@@ -47,16 +127,12 @@ class LUNode
   def to_smarts
     l_s = @lab_n.split
     if l_s.size > 1 
-        print "["
         for i in 0..l_s.size-1 do
             print "##{l_s[i]}"
-            if i<l_s.size-1 
-                print "," 
-            end
+            print "," unless i==l_s.size-1
         end
-        print "]"
     else 
-        print "[##{@lab_n}]"
+        print "##{@lab_n}"
     end
   end
 end
@@ -73,10 +149,19 @@ class LUEdge
   def to_smarts
      l_e = @lab_e.split
     if l_e.size > 1 
-        print "~" 
+        for i in 0..l_e.size-1
+            case l_e[i]
+                when '1' then print "-"
+                when '2' then print "="
+                when '3' then print "#"
+                else 
+            end
+            print "," unless i==l_e.size-1
+        end
     else 
         case @lab_e
-            when '1' then print "-"
+            # Uncomment the next line for explicit aliphatic bindings in notation
+            #when '1' then print "-"
             when '2' then print "="
             when '3' then print "#"
             else 
@@ -162,13 +247,80 @@ end
 def smarts(dom)
     dom[:grps].sort{|a,b| a[0]<=>b[0]}.each do |id, g| 
         print "#{id}\t"
-        g.to_smarts(0,0)
+        g.to_smarts(0,0,0)
         print "\n"
     end
 end
 
 
+def match (smiles, smarts)
+    c=OpenBabel::OBConversion.new
+    c.set_in_format 'smi'
+    m=OpenBabel::OBMol.new
+    c.read_string m, smiles
+    m.set_aromatic_perceived
+
+    p=OpenBabel::OBSmartsPattern.new
+    if !p.init(smarts)
+        puts "Error! Smarts pattern invalid."
+        exit
+    end
+    p.match(m,true)
+    
+    p.match(m)
+    hits = p.get_umap_list
+    print "Found #{hits.size} instances of the SMARTS pattern '#{smarts}' in the SMILES string '#{smiles}'."
+    if hits.size>0
+        puts " Here are the atom indices:"
+    else
+        print "\n"
+    end
+    hits.each_with_index do |hit, index|
+        print "  Hit #{index}: [ "
+        hit.each do |atom_index|
+            print "#{atom_index} "
+        end
+        puts "]"
+    end		
+    
+end
+
 # Main
 STDOUT.sync = true
 dom = read
 smarts(dom)
+
+
+
+# Demonstrates different SMARTS patterns
+def demo
+    puts
+    # This pattern is equivalent to  [#7][#7][#6] :(
+    match("NNC",                "[$([#7]),$([#7]=[#8])][$([#7]),$([#7][#6][#6]),$([#7][#6])][$([#6]),$([#6][#7]),$([#6]~[#7,#8])]")    # yes (no)
+    match("N(=O)NC",            "[$([#7]),$([#7]=[#8])][$([#7]),$([#7][#6][#6]),$([#7][#6])][$([#6]),$([#6][#7]),$([#6]~[#7,#8])]")    # yes (no)
+    match("N(=O)NCN",           "[$([#7]),$([#7]=[#8])][$([#7]),$([#7][#6][#6]),$([#7][#6])][$([#6]),$([#6][#7]),$([#6]~[#7,#8])]")    # yes (no)
+    match("N(=O)NC(N)N",        "[$([#7]),$([#7]=[#8])][$([#7]),$([#7][#6][#6]),$([#7][#6])][$([#6]),$([#6][#7]),$([#6]~[#7,#8])]")    # yes (no)
+    match("N(=O)NC=O",          "[$([#7]),$([#7]=[#8])][$([#7]),$([#7][#6][#6]),$([#7][#6])][$([#6]),$([#6][#7]),$([#6]~[#7,#8])]")    # yes (no)
+    match("N(=O)NC(N)=O",       "[$([#7]),$([#7]=[#8])][$([#7]),$([#7][#6][#6]),$([#7][#6])][$([#6]),$([#6][#7]),$([#6]~[#7,#8])]")    # yes (yes)
+
+    puts
+    # This pattern is better (seems to demand a branch), but actually demands no branch since no explict degree is forced, which allows "folding" :(
+    match("NNC",            "[#7][$([#7][#6][#6]),$([#7][#6])][$([#6][#7]),$([#6]~[#7,#8])]")                         # yes (no)
+    match("NN(C)C",         "[#7][$([#7][#6][#6]),$([#7][#6])][$([#6][#7]),$([#6]~[#7,#8])]")                         # yes (no)
+    match("NN(C)C(N)",      "[#7][$([#7][#6][#6]),$([#7][#6])][$([#6][#7]),$([#6]~[#7,#8])]")                         # yes (yes)
+    match("NN(C)C(=O)",     "[#7][$([#7][#6][#6]),$([#7][#6])][$([#6][#7]),$([#6]~[#7,#8])]")                         # yes (yes)
+    match("NN(CC)(C)C(=O)", "[#7][$([#7][#6][#6]),$([#7][#6])][$([#6][#7]),$([#6]~[#7,#8])]")                         # yes (yes)
+    match("NNC(=O)",        "[#7][$([#7][#6][#6]),$([#7][#6])][$([#6][#7]),$([#6]~[#7,#8])]")                         # yes (no)
+
+    puts
+    # This enhanced pattern enforces 1-step environments around the current node (f) and requires at least one branch :)
+    match("NNC",                     "[#7][#7;$([#7]([#6][#6])([#7])[#6]),$([#7]([#6])([#7])[#6])](~*)[#6;$([#6]([#7])[#7]),$([#6](-,=[#7,#8])[#7])](~*)") # no (no)
+    match("NN(C)C",                  "[#7][#7;$([#7]([#6][#6])([#7])[#6]),$([#7]([#6])([#7])[#6])](~*)[#6;$([#6]([#7])[#7]),$([#6](-,=[#7,#8])[#7])](~*)") # no (no)
+    match("NNC(N)",                  "[#7][#7;$([#7]([#6][#6])([#7])[#6]),$([#7]([#6])([#7])[#6])](~*)[#6;$([#6]([#7])[#7]),$([#6](-,=[#7,#8])[#7])](~*)") # no (no)
+    match("NN(C)C(=N)",              "[#7][#7;$([#7]([#6][#6])([#7])[#6]),$([#7]([#6])([#7])[#6])](~*)[#6;$([#6]([#7])[#7]),$([#6](-,=[#7,#8])[#7])](~*)") # yes (yes)
+    match("NN(CC)(C)C(N)",           "[#7][#7;$([#7]([#6][#6])([#7])[#6]),$([#7]([#6])([#7])[#6])](~*)[#6;$([#6]([#7])[#7]),$([#6](-,=[#7,#8])[#7])](~*)") # yes (yes)
+    match("NN(CC)(C)C(N)(=O)",       "[#7][#7;$([#7]([#6][#6])([#7])[#6]),$([#7]([#6])([#7])[#6])](~*)[#6;$([#6]([#7])[#7]),$([#6](-,=[#7,#8])[#7])](~*)") # yes (yes)
+    match("NN(CC)C(=C)",             "[#7][#7;$([#7]([#6][#6])([#7])[#6]),$([#7]([#6])([#7])[#6])](~*)[#6;$([#6]([#7])[#7]),$([#6](-,=[#7,#8])[#7])](~*)") # no (no)
+    match("NN(CN)C(=N)",             "[#7][#7;$([#7]([#6][#6])([#7])[#6]),$([#7]([#6])([#7])[#6])](~*)[#6;$([#6]([#7])[#7]),$([#6](-,=[#7,#8])[#7])](~*)") # yes (yes) <- two embeddings is correct
+    match("NN(N)C(=N)",              "[#7][#7;$([#7]([#6][#6])([#7])[#6]),$([#7]([#6])([#7])[#6])](~*)[#6;$([#6]([#7])[#7]),$([#6](-,=[#7,#8])[#7])](~*)") # no (no)
+end
